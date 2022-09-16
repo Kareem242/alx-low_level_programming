882f410ec34587fbd92d3c28230ecd369cab1487
Kareem242 <mohammedkareem242@gmail.com>
2022-09-16 07:17:26 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * * print_numbers - a function that multiplies two integers
+ * * Return: void
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar('0' + a);
+	}
+	_putchar('\n');
+}
