e97f3df8129fb8d29efe9ace4b4509967b8ac5a5
Kareem242 <mohammedkareem242@gmail.com>
2022-09-21 05:27:30 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ ** reset_to_98 - function checks for upper case.
+ ** _isupper - checks if the letters are uppercase
+ **  @n: input.
+ **
+ **  Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
