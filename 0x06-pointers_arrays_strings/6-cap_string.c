ff2642ec1781a9ec4ee072aae46d71abb85a95a6
Kareem242 <mohammedkareem242@gmail.com>
2022-09-22 07:07:20 -0700

    Pointers and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,13 +26,13 @@ char *cap_string(char *str)
 			str[index - 1] == '?' ||
 			str[index - 1] == '"' ||
 			str[index - 1] == '(' ||
-			str[index - 1] == ')' ||	
+			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
-			str[index] -= 32;
-			index++;
-		}
+				str[index] -= 32;
+		index++;
+	}
 
-			return (str);
+	return (str);
 }
